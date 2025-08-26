19d6d2646fd57ec07270968e6364c6b3125ce496
Xspark <151998806+xsprk@users.noreply.github.com>
2025-08-26 14:39:46 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,7 @@ export default function Footer() {
         <p
           className="text-sm font-semibold bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent"
         >
-          Download from Instagram
-        </p>
-        <p className="text-xs text-gray-500 dark:text-gray-400 mt-1">
-          © {new Date().getFullYear()} XDL — All rights reserved.
+          © 2020-{new Date().getFullYear()} 1NE Networks. All rights reserved.
         </p>
       </div>
     </footer>
